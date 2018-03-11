ff9c2ed14a69cdf03bfc50cf95c10ce10859794d
danielfsokol <danielfsokol@gmail.com>
2018-03-11 10:01:30 -0400

    complete initialize checkpoint

--- app/models/user.rb
+++
@@ -1,8 +0,0 @@
-class User < ApplicationRecord
-  attr_accessor :response
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
-
-end
--- lib/kele.rb
+++ lib/kele.rb
@@ -1,17 +1,15 @@
 require 'httparty'
 
-class Keles
+class Kele
   include HTTParty
-
-  attr_accessor :response
 
   def initialize(email, password)
     @email = email
     @password = password
     @bloc_api = "https://www.bloc.io/api/v1"
-    @sessions_url = "https://www.bloc.io/api/v1/sessions"
+    @sessions_url = "#{@bloc_api}/sessions"
     @response = self.class.post(@sessions_url, :query => {email: @email, password: @password})
+    @auth_token = @response["auth_token"]
   end
-
 
 end
