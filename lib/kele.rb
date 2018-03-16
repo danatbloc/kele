f575ec6f87c307311637ea5bb51ee6d4b0c76b91
danielfsokol <danielfsokol@gmail.com>
2018-03-16 09:59:04 -0400

    complete messaging checkpoint

--- lib/kele.rb
+++ lib/kele.rb
@@ -11,7 +11,7 @@ class Kele
     @password = password
     @bloc_api = "https://www.bloc.io/api/v1"
     @sessions_url = "#{@bloc_api}/sessions"
-    @response = self.class.post(@sessions_url, :query => {email: @email, password: @password})
+    @response = self.class.post(@sessions_url, body: {email: @email, password: @password})
     @auth_token = @response["auth_token"]
   end
 
@@ -23,6 +23,22 @@ class Kele
   def get_mentor_availability(mentor_id)
     response = self.class.get("#{@bloc_api}/mentors/#{mentor_id}/student_availability", headers: { "authorization" => @auth_token })
     response.to_a
+  end
+
+  def get_messages(page_number=1)
+    response = self.class.get("#{@bloc_api}/message_threads", headers: { "authorization" => @auth_token }, body: { "page": page_number})
+    JSON.parse(response.body)
+  end
+
+  def create_message(sender, recipient_id, subject, message)
+    self.class.post("#{@bloc_api}/messages",
+    headers: { "authorization" => @auth_token },
+    body: {
+      "sender": sender,
+      "recipient_id": recipient_id,
+      "subject": subject,
+      "stripped-text": message.strip
+    })
   end
 
 end
